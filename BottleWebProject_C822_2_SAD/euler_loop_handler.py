f0803787469a3aa0d6dafba1b923565f7fbe9021
Dimitor <mpovelit@gmail.com>
2021-06-01 14:24:01 +0300

    add error handlers

--- BottleWebProject_C822_2_SAD/euler_loop_handler.py
+++ BottleWebProject_C822_2_SAD/euler_loop_handler.py
@@ -7,9 +7,14 @@ from euler_loop_search import find_euler_loop
 
 @post('/eulerian_loop', method='post')
 def search():
-    matrix_dim = int(request.forms.get("MATRIX"))
+    try:
+        matrix_dim = int(request.forms.get("MATRIX"))
+    except:
+        return template("error", message='Количество вершин не введено!')
+
     if matrix_dim < 1:
         return template("error", message='Вершин должно быть не меньше одной!')
+
     return template('enter_matrix', title='Поиск эйлерового цикла в графе',
                     message='Задача Дмитрия Повеличенко на поиск эйлерового цикла в графе',
                     year=datetime.now().year, rows=matrix_dim, columns=matrix_dim,
