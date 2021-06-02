dcb72a8e68c8a4742b9047f574db8a8216c64b36
MrLargha <cppcodery@gmail.com>
2021-06-02 13:23:29 +0300

    Added unittest impl

--- BottleWebProject_C822_2_SAD/test_subgraph_finder.py
+++ BottleWebProject_C822_2_SAD/test_subgraph_finder.py
@@ -1,15 +1,40 @@
 from unittest import TestCase
+from subgraph_finder import ClickFinder
+from graph import Graph
 
 
 class TestClickFinder(TestCase):
 
     """Unittest data"""
-
-    def test_contains_all_connected(self):
-        self.fail()
+    big_graph = Graph([[1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+                       [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+                       [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1],
+                       [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
+                       [0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                       [0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+                       [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+                       [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0]])
 
     def test_find_clicks(self):
-        self.fail()
+        finder = ClickFinder(self.big_graph)
+        result = finder.find_clicks()
+        self.assertTrue(len(result) == 5)
+        self.assertTrue(result[0] == self.big_graph.nodes[0:5])
+        # TODO: Implement other equations
 
     def test_save_result_to_file(self):
-        self.fail()
+        finder = ClickFinder(self.big_graph)
+        finder.find_clicks()
+        path = finder.save_result_to_file()
+        try:
+            f = open(path, 'r')
+            return
+        except Exception:
+            self.fail("File is not saved correctly")
